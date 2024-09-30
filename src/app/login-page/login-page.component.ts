34db68803255cdf1f7aa08f5ce5076aa392fe195
Ralph Marvin <ralphmarvin4@gmail.com>
2024-09-30 15:25:29 +0000

    first commit

---
+++ src/app/login-page/login-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-login-page',
+  styleUrl: './login-page.component.css',
+  templateUrl: './login-page.component.html',
+  imports: [],
+})
+export class LoginPageComponent {
+
+}
